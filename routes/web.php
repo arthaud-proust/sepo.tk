de91d1f89e2ce09a2c69d090f266d4db2e2f965e
Arthaud Proust <arthaudp33@gmail.com>
2020-05-16 12:40:00 +0200

    fix SAP

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '^((?!api).)*');
